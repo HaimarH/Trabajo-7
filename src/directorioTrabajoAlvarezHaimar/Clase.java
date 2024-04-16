0f0c5680a567719af2b53b389d6eb902bf1345ad
unknown <DAM101@iesataulfo.local>
2024-04-16 15:53:36 +0200

    Primera

---
+++ src/directorioTrabajoAlvarezHaimar/Clase.java
@@ -0,0 +1,9 @@
+package directorioTrabajoAlvarezHaimar;
+
+public class Clase {
+
+	
+	public Clase() {
+		System.out.println("Soy un objeto de la clase y acabo de ser creado");
+	}
+}
---
+++ src/directorioTrabajoAlvarezHaimar/Principal.java
@@ -0,0 +1,12 @@
+package directorioTrabajoAlvarezHaimar;
+
+public class Principal {
+	//Creamos el metodo main se encargara de crear un objeto de la clase Clase
+	public static void main(String[] args) {
+	//Creamos el objeto de la clase Clase	
+		Clase clase=new Clase();
+
+		
+	}
+
+}
